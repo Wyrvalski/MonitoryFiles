d6a005bd1279bbfa72f74c378818d4466d034b9e
Unknown <unknown@example.com>
2020-07-26 14:45:49 -0300

    refactor(monitory-changes): observar todos arquivos adicionados juntos

--- src/main/java/main/MonitoryFiles.java
+++ src/main/java/main/MonitoryFiles.java
@@ -1,21 +1,23 @@
 package main;
-
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 
 public class MonitoryFiles {
     public static void main(String[] args){
-        Path inDirectory = Paths.get(System.getProperty("user.home").concat(File.separator).concat("data").
-                concat(File.separator).
-                concat("in"));
-        System.out.println(inDirectory);
+
+        Path inDirectory = createPath("in");
+        Path outDirectory = createPath("out");
 
         while (true) {
             try(WatchService watchService = FileSystems.getDefault().newWatchService()){
                 inDirectory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
                 WatchKey key = watchService.take();
                 Thread.sleep(1000);
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    String filename = event.context().toString();
+                }
+
             } catch (IOException ex) {
 
             } catch (InterruptedException ex) {
@@ -23,5 +25,13 @@ public class MonitoryFiles {
             }
 
         }
+    }
+
+    public static Path createPath( String inOut) {
+        return (Paths.get(
+                System.getProperty("user.home").concat(File.separator).concat("data").
+                        concat(File.separator).
+                        concat(inOut)
+        ));
     }
 }
