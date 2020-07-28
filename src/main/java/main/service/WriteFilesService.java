8fc3bd8244182ba9265e3782408ad5ab7ec68ce3
Eduardo Duarte <dudu_duarte@hotmail.com>
2020-07-28 20:21:09 -0300

    fix(write): corrigir problemas com charset.

--- src/main/java/main/service/WriteFilesService.java
+++ src/main/java/main/service/WriteFilesService.java
@@ -29,13 +29,7 @@ public class WriteFilesService {
     public void writeOnFile (Path outDirectory, WatchEvent<?> event) {
 
         String filename = LocalDate.now().format(DateTimeFormatter.ofPattern("d-MM-uuuu")) + "-RelatiorioDetalhado.done.dat";
-        try  {
-            BufferedWriter writer;
-            if (System.getProperty("os.name").contains("Windows")) {
-                writer = Files.newBufferedWriter(outDirectory.resolve(filename), ISO_8859_1);
-            } else {
-                writer = Files.newBufferedWriter(outDirectory.resolve(filename));
-            }
+        try (BufferedWriter writer = Files.newBufferedWriter(outDirectory.resolve(filename), ISO_8859_1)) {
             writer.write(mountOutPutFile(this.allDataInFile));
         } catch (IOException ex) {
 
